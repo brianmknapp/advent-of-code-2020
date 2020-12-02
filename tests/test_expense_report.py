df9284c260d20a1beea7bc2dd8b2ea365dee03e2
Brian Knapp <brianmknapp@gmail.com>
2020-12-01 21:41:01 -0500

    Complete part 2

--- tests/test_expense_report.py
+++ tests/test_expense_report.py
@@ -3,7 +3,14 @@ import pytest
 from expense_report import expense_report
 
 
-def test_main():
+def test_two_entries_that_sum():
     first_number, second_number = expense_report.two_entries_that_sum([1721, 979, 366, 299, 675, 1456], 2020)
     assert first_number == 1721
     assert second_number == 299
+
+
+def test_three_entries_that_sum():
+    first_number, second_number, third_number = expense_report.three_entries_that_sum([1721, 979, 366, 299, 675, 1456], 2020)
+    assert first_number == 979
+    assert second_number == 366
+    assert third_number == 675
